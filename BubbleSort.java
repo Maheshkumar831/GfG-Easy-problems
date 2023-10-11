c2ff9e9999ce1903f6687ecf641d0eb49161bf55
Mahesh Kumar R <114941303+Maheshkumar831@users.noreply.github.com>
2023-10-11 06:27:31 +0530

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,29 @@
+class Solution
+{
+    //Function to sort the array using bubble sort algorithm.
+	public static void bubbleSort(int arr[], int n)
+    {
+        for(int i=n-1;i>0;i--){
+            for(int j=0;j<i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+}
+/*
+Given an Integer N and a list arr. Sort the array using bubble sort algorithm.
+Example 1:
+
+Input: 
+N = 5
+arr[] = {4, 1, 3, 9, 7}
+Output: 
+1 3 4 7 9
+
+Expected Time Complexity: O(N^2).
+Expected Auxiliary Space: O(1).
+  */
