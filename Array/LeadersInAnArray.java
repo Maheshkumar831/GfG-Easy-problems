d3df7d9316a4eb2d47179f2f551169febfc0afc2
Mahesh Kumar R <114941303+Maheshkumar831@users.noreply.github.com>
2023-11-19 09:45:45 +0530

    Update LeadersInAnArray.java

--- Array/LeadersInAnArray.java
+++ Array/LeadersInAnArray.java
@@ -2,6 +2,24 @@ class Solution{
     //Function to find the leaders in the array.
     static ArrayList<Integer> leaders(int arr[], int n){
         ArrayList<Integer>list=new ArrayList<Integer>();
+        int max=arr[n-1];
+        list.add(arr[n-1]);
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]>=max){
+                list.add(arr[i]);
+                max=arr[i];
+            }
+        }
+        Collections.reverse(list);
+        return list;
+        
+    }
+}
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        ArrayList<Integer>list=new ArrayList<Integer>();
         int flag=0;
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
